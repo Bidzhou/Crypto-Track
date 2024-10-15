ae8d08b6fd291e63abbd115dd5239a0a48884a09
Bidzhou <89260226211@mail.ru>
2024-10-15 15:18:33 +0300

    Initial Commit

---
+++ CryptoProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoProject
+//
+//  Created by Frederico del' Bidzho on 15.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoProject/CryptoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoProjectApp.swift
+//  CryptoProject
+//
+//  Created by Frederico del' Bidzho on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
